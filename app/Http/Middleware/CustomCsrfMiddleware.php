f566fbb62e16528724af4eef4f1d7b29ecc0edb5
Aizawa <sj.116tlgb.pro418@gmail.com>
2025-02-27 15:30:42 +0900

    CSRF設定4

--- app/Http/Middleware/CustomCsrfMiddleware.php
+++ app/Http/Middleware/CustomCsrfMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CustomCsrfMiddleware
@@ -13,9 +14,13 @@ class CustomCsrfMiddleware
         // ヘッダーからCSRFトークンを取得
         $csrfToken = $request->header('X-XSRF-TOKEN');
 
+        Log::info('Received CSRF Token:', ['csrf_token' => $csrfToken]);
+        Log::info('Expected CSRF Token:', ['expected_token' => csrf_token()]);
+
+
         // CSRFトークンが一致するか確認
         if (!$csrfToken || !hash_equals(csrf_token(), $csrfToken)) {
-            
+            Log::error('Invalid CSRF Token');
             // トークンが一致しない場合は403エラーを返す
             return response()->json(['error' => '無効なトークンです'], Response::HTTP_FORBIDDEN);
         }
